2cecd409237458f366de59f9c06315a65734f1ed
EgorYukha <yukha90@mail.ru>
2022-09-02 18:01:20 +0300

    Добавлено ДЗ семинар 2

---
+++ Seminar_2/Task_1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход случайно сгенерированное трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int rand = new Random().Next(100, 999);
+Console.WriteLine(rand);
+int digit_second = (rand % 100) / 10;
+Console.WriteLine(digit_second);
---
+++ Seminar_2/Task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход случайно сгенерированное число (от 1 до 1 000 000) и проверяет, кратно ли оно одновременно 7 и 23.
+int start = 1;
+int stop = 1000000;
+int rand = new Random().Next(start, stop);
+//int rand = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rand);
+if(rand % 7 == 0)
+{
+    Console.WriteLine("Число кратное 7.");
+}
+else
+{
+    Console.WriteLine("Условие не выполняется. Не кратное 7.");
+}
+if(rand % 23 == 0)
+{
+    Console.WriteLine("Число кратное 23.");
+}
+else
+{
+    Console.WriteLine("Условие не выполняется. Не кратное 23.");
+}
