b2df13d17d54522e8658303d5c800402d4f7ed84
EgorYukha <yukha90@mail.ru>
2022-09-08 16:33:42 +0300

    add dz 4

---
+++ Seminar_4/Task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A 
+//в натуральную степень B. Не использовать функцию Math.Pow()
+
+Console.WriteLine("Введите первое число A: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число B: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+//ver.1. 
+//int degree = Convert.ToInt32(Math.Pow(num1, num2));
+//Console.WriteLine(degree);
+
+//ver.2.
+//Console.WriteLine($"{num1} в степени {num2} = " + Math.Pow(num1, num2));
+
+int degree = num1;
+
+for (int i = 1; i < num2; i++)
+{
+degree = degree * num1;
+}
+Console.WriteLine($"{num1} в степени {num2} = "  + degree);
---
+++ Seminar_4/forMe/Program.cs
@@ -0,0 +1,27 @@
+﻿//1.
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+//2.
+int countOfDigits = 0;
+int tmp = num;
+while(tmp != 0)
+{
+    tmp = tmp / 10;
+    countOfDigits++;
+}
+// countOfDigits = num.Tostring().Lenght;
+//3.
+int[] array;
+array = new int [countOfDigits];
+
+//4.
+int i = 0;
+tmp = num;
+while(tmp != 0)
+{
+    array[i] = tmp % 10;
+    tmp /= 10;
+    i++;
+}
+//5.
+Console.WriteLine();
