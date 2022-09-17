bc78414db0088ef135cf9144b67431f89a25650c
EgorYukha <yukha90@mail.ru>
2022-09-17 13:22:02 +0300

    dz 6

---
+++ Seminar_6/Task_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 1: Программа запрашивает натуральное число M, пользователь вводит с клавиатуры 
+// M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введи количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+
+void InputNumbers(int m)
+{
+for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int counting(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if(array[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine($"Введено больше 0 чисел: {counting(array)} ");
---
+++ Seminar_6/Task_3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 3: * Найдите сумму произведений пар чисел в одномерном целочисленном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. Если кол-во 
+// элементов нечетное, то элемент посередине не учитывать.
+// Результат сложения вывести на экран.
+// 1, 3, 8, 3, 2 -> 11
+// 8, 3, 4, 2 -> 28
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+    Console.Write(array[i] + ", ");
+}
+Console.WriteLine("");
+int result = 1;
+//int sum = 0;
+//int last = array.Last();
+//int first = array.First();
+for (int i = 0; i < array.Length; i++)
+{
+    result = array[i] * array[^1];
+}
+Console.WriteLine("произвед " + result);
+
+// int[] numbers = { 1, 2, 3, 4, 5 };
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     numbers[i] = numbers[i] * 2;
+//     Console.WriteLine(numbers[i]);
+// }
